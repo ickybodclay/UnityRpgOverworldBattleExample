0c6a88c2a96ff7f9bae17e19b21617d6000eab81
Jason Petterson <jp@brokenshotgun.com>
2018-06-12 19:10:01 -0400

    added player and simple overworld movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    private float speed = 3f;
+
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	void Update () {
+        HandleInput();
+	}
+
+    private void HandleInput() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += h * speed * Time.deltaTime;
+        pos.y += v * speed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
